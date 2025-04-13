a1189d66a4647944f88465daf801a5fd9e714cb1
Vieira Neto <vieira.neto@classdojo.com>
2025-04-13 17:20:09 +0000

    resolve sequence for and and or goal

---
+++ examples/prismModels/dtmc_normalized_new_goals.r
@@ -0,0 +1,71 @@
+dtmc
+
+module G0
+  G0pursued: [0..1] init 0;
+  G0achieved: [0..1] init 0;
+
+  [pursueG0] G0pursued=0 & G0achieved=0 -> (G0pursued'=1);
+
+  [pursueG1] G0pursued=1 -> true;
+  [pursueG2] G0pursued=1 & outside -> true;
+  [pursue_maintain] G0pursued=1 & !achieved_maintain -> true;
+endmodule
+
+formula achieved_maintain = outside=true;
+
+module G1
+  G1pursued: [0..1] init 0;
+  G1achieved: [0..1] init 0;
+
+  [pursueG1] G1pursued=0 & G1achieved=0 -> (G1pursued'=1);
+  [pursueG3] G1pursued=1 & G4pursued=0 & G4failed > 1 -> true;
+  [pursueG4] G1pursued=1 & G3pursued=0 -> true;
+
+  [achievedG1] G1pursued=1 & (G3achieved=1 | G4achieved=1) -> (G1pursued'=0) & (G1achieved'=1);
+  [skipG1] G1pursued=1 -> (G1pursued'=0);
+endmodule
+
+module G3
+  G3pursued: [0..1] init 0;
+  G3achieved: [0..1] init 0;
+
+  [pursueG3] G3pursued=0 & G3achieved=0 -> (G3pursued'=1);
+  [achievedG3] G3pursued=1 -> (G3pursued'=0) & (G3achieved'=1);
+  [skipG3] G3pursued=1 -> (G3pursued'=0);
+endmodule
+
+module G4
+  G4pursued: [0..1] init 0;
+  G4achieved: [0..1] init 0;
+  G4failed: [0..10] init 0;
+
+  [pursueG4] G4pursued=0 & G4achieved=0 -> (G4pursued'=1);
+  [achievedG4] G4pursued=1 -> (G4pursued'=0) & (G4achieved'=1);
+  [skipG4] G4pursued=1 -> (G4pursued'=0);
+  [pursueG6] G4pursued=1 & G6achieved=0 -> true;
+  [pursueG7] G4pursued=1 & G6achieved=1 -> true;
+endmodule
+
+module G2
+  G2pursued: [0..1] init 0;
+  G2achieved: [0..1] init 0;
+
+  [pursueG2] G2pursued=0 & G2achieved=0 & outside=true -> (G2pursued'=1);
+endmodule
+
+module G10
+  G10pursued: [0..1] init 0;
+
+  [pursueG10]  G10pursued=0 -> (G10pursued'=1);
+  [pursueG11]  G10pursued=1 & G12pursued=0 & G12achieved=0 -> true;
+  [pursueG12]  G10pursued=1 & G11pursued=0 & G11achieved=0 -> true;
+endmodule
+
+module ChangeManager
+  [pursueT1] true -> true;
+  [achievedT1] true -> true;
+endmodule
+
+module System
+  outside: bool init false;
+endmodule
--- src/dtmc/modules/goalModule.ts
+++ src/dtmc/modules/goalModule.ts
@@ -1,11 +1,96 @@
-import { achieved, pursued } from '../../mdp/common';
+import { achieved, pursued, separator } from '../../mdp/common';
 import { GoalNode, GoalNodeWithParent } from '../../ObjectiveTree/types';
+
+const beenAchieved = (
+  goalId: string,
+  { condition }: { condition: boolean }
+) => {
+  return `${achieved(goalId)}=${condition ? 1 : 0}`;
+};
+
+const beenPursued = (goalId: string, { condition }: { condition: boolean }) => {
+  return `${pursued(goalId)}=${condition ? 1 : 0}`;
+};
+
+const beenAchievedAndPursued = (
+  goalId: string,
+  {
+    achievedCondition,
+    pursuedCondition,
+  }: { achievedCondition: boolean; pursuedCondition: boolean }
+) => {
+  return [
+    beenPursued(goalId, { condition: pursuedCondition }),
+    beenAchieved(goalId, { condition: achievedCondition }),
+  ].join(' & ');
+};
+
+const pursueSequentialGoal = (
+  goal: GoalNode,
+  sequence: string[],
+  childId: string
+): string => {
+  const sequenceIndex = sequence.indexOf(childId);
+  const leftGoals = sequence.slice(0, sequenceIndex);
+  const rightGoals = sequence.slice(sequenceIndex + 1);
+
+  if (!goal.relationToChildren) {
+    return '';
+  }
+
+  const resolveAndGoal = (): string => {
+    if (leftGoals.length === 0) {
+      return [
+        beenAchieved(childId, { condition: false }),
+        rightGoals
+          .map((goal) => beenAchieved(goal, { condition: false }))
+          .join(separator('and')),
+      ].join(separator('and'));
+    }
+
+    return [
+      ...leftGoals.map((goal) => beenAchieved(goal, { condition: true })),
+      ...rightGoals.map((goal) => beenAchieved(goal, { condition: false })),
+    ].join(separator('and'));
+  };
+
+  const resolveOrGoal = () => {
+    if (leftGoals.length === 0) {
+      return rightGoals
+        .map((goal) =>
+          beenAchievedAndPursued(goal, {
+            pursuedCondition: false,
+            achievedCondition: false,
+          })
+        )
+        .join(separator('and'));
+    }
+    return leftGoals
+      .map((goal) =>
+        beenAchievedAndPursued(goal, {
+          pursuedCondition: true,
+          achievedCondition: false,
+        })
+      )
+      .join(separator('and'));
+  };
+
+  if (goal.relationToChildren === 'and') {
+    return resolveAndGoal();
+  }
+
+  if (goal.relationToChildren === 'or') {
+    return resolveOrGoal();
+  }
+
+  return '';
+};
 
 const pursueStatements = (goal: GoalNode): string[] => {
   const goalsToPursue = [goal, ...(goal.children || [])];
 
   const pursueLines = goalsToPursue
-    .map((child): [GoalNode, { left: string; right: string }] => {
+    .map((child, _, arr): [GoalNode, { left: string; right: string }] => {
       const isItself = child.id === goal.id;
       const leftStatement = `[pursue_${child.id}] ${pursued(goal.id)}=${isItself ? 0 : 1}`;
       if (isItself) {
@@ -18,7 +103,21 @@ const pursueStatements = (goal: GoalNode): string[] => {
         ] as const;
       }
 
-      return [child, { left: leftStatement, right: `true` }] as const;
+      if (goal.executionDetail?.type === 'sequence') {
+        const pursueCondition = pursueSequentialGoal(
+          goal,
+          goal.executionDetail.sequence,
+          child.id
+        );
+        return [
+          child,
+          {
+            left: leftStatement + ` & ${pursueCondition}`,
+            right: 'true',
+          },
+        ] as const;
+      }
+      return [child, { left: leftStatement, right: leftStatement }] as const;
     })
     .map(([child, statement]): { left: string; right: string } => {
       return {
