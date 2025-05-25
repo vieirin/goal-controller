1e407f896f906c6242d0941cc4e989544364048e
Vieira Neto <vieira.neto@classdojo.com>
2025-05-25 15:25:57 +0000

    add resources as variables

--- src/ObjectiveTree/treeVariables.ts
+++ src/ObjectiveTree/treeVariables.ts
@@ -5,6 +5,19 @@ const getTreeVariables = (tree: GoalTreeWithParent) => {
   const variables = new Set<string>();
 
   const goals = allByType({ gm: tree, type: 'goal' });
+  const resources = allByType({ gm: tree, type: 'resource' });
+
+  resources.forEach((resource) => {
+    if (resource.name) {
+      const sanitizedName = resource.name
+        .toLocaleLowerCase()
+        .replace(/[^a-zA-Z0-9]/g, '_');
+      const variableName = `resource_${sanitizedName}`;
+      variables.add(variableName);
+    } else {
+      throw new Error('Resource name is required');
+    }
+  });
 
   goals.forEach((goal) => {
     goal.maintainCondition?.maintain.variables.forEach((variable) => {
